c853ac3bbda5e18f69a3073e433e5d1c8dbf05d3
Marina <95050756+Marindala@users.noreply.github.com>
2023-07-29 04:47:13 -0300

    canvas-components-pages and store

---
+++ src/pages/Home.jsx
@@ -0,0 +1,59 @@
+//rafce
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import CustomButton from "../components/CustomButton";
+
+import state from "../store";
+
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./logo.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                COSAS DE CHICAS <br className="xl:block hidden" /> GAMERS.
+              </h1>
+              <motion.div
+                {...headContentAnimation}
+                className="flex flex-col gap-5"
+              >
+                <p className="max-w-md font-normal text-gray-600 text-base">
+                  Crea tu camiseta única y exclusiva con nuestra nueva
+                  herramienta de personalización en 3D.{" "}
+                  <strong>Da rienda suelta a tu imaginación</strong> y define tu
+                  propio estilo
+                </p>
+
+                <CustomButton
+                  type="filled"
+                  title="Personalizarlo"
+                  handleClick={() => (state.intro = false)}
+                  customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                />
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
